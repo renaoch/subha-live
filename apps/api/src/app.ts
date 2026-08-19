508e71eee860c12f531bbb51d3b867259542ac1e
Renao Chetri <chetriprem.work@gmail.com>
2026-08-19 10:40:34 +0530

    Feat: Added Cors origin

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import healthRoutes from "./routes/health";
 import authRoutes from "./modules/auth/auth.routes";
@@ -7,8 +8,20 @@ import socialRoutes from "./modules/social/social.routes";
 import { errorMiddleware } from "./middleware/error.middleware";
 import roomRoutes from "./modules/rooms/room.routes";
 
+
 const app = express();
 
+app.use(
+  cors({
+    origin: [
+      "https://www.subha.fun",
+      "https://subha.fun",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 app.use(express.json());
 
 app.use("/health", healthRoutes);
